ad3b1094e07b515f84dceac0be5c420bd54c2b96
rajesh <ajoypal.raj@gmail.com>
2024-11-17 11:22:32 +0600

    c7: JS operators

---
+++ 07/14.js
@@ -0,0 +1,69 @@
+/**
+ * ! JS Operators
+ * *JavaScript operators are symbols that are used to perform operations on operands
+ * JAVASCRIPT OPERATORS:
+ * (i) Arithmetic Operators
+ * (ii) Comparison (Relational) Operators
+ * (iii) Bitwise Operators
+ * (iv) Logical Operators
+ * (v) Assignment Operators
+ * (vi) Special Operators
+ */
+
+
+/**
+ * ! * (i) Arithmetic Operators
+ * *Arithmetic operators are used to perform arithmetic operations on the operands. The following operators are known as JavaScript arithmetic operators.
+ * 1. +
+ * 2. -
+ * 3. *
+ * 4. /
+ * 5. %
+ * 6. ++
+ * 7. --
+ */
+
+/**
+ * !(ii) Comparison (Relational) Operators
+ * *The JavaScript comparison operator compares the two operands. The comparison operators are as follows:
+ * 1.==  -> is equal to 
+ * 2.=== -> identical(equal and of same type)
+ * 3.!=  -> Not equal to
+ * 4.!== -> Not identical 
+ * 5.>   -> Greater than
+ * 6.>=  -> Greater than or equal to 
+ * 7.<   -> Less than
+ * 8.<=  -> Less than or equal to
+ */
+
+/**
+ * !(iii) Bitwise Operators
+ * *The bitwise operators perform bitwise operations on operands. The bitwise operators are as follows:
+ * 1.&  -> Bitwise And
+ * 2.|  -> Bitwise Or
+ * 3.^  -> Bitwise Xor
+ * 4.~  -> Bitwise Not
+ * 5.<< -> Bitwise Left shift
+ * 6.>> -> Bitwise right shift
+ * 7.>>> -> Bitwise Right shift zero
+ */
+
+/**
+ * !(iv) Logical Operators
+ * *The following operators are known as JavaScript logical operators
+ * 1. && -> Logical And
+ * 2. || -> Logical Or
+ * 3. !  -> Logical Not
+ */
+
+/**
+ * !(v) Assignment Operators
+ * *The following operators are known as JavaScript assignment operators
+ * 1. =  -> assign
+ * 2.+=  -> Add and Assign
+ * 3.-=  -> Subtract and Assign
+ * 4.*=  -> Multiply and Assign
+ * 5./=  -> Devide and Assign
+ * 6.%=  -> Modulus and assign
+ * 
+ */
