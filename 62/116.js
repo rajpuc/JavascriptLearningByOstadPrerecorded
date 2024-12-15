bb95430219b70e486cbfa6c460bee4702c55089b
rajesh <ajoypal.raj@gmail.com>
2024-12-15 22:17:59 +0600

    c62: consturcor method in class

---
+++ 62/116.js
@@ -0,0 +1,24 @@
+/**
+ * ! Class constructor:
+ */
+
+// constructor bujar jonno first a akta class likhe nei.
+
+class Person{
+
+    // class er moddhe constructor namer jodi amra kono method likhi shetai hocce constructor method.
+    constructor(){
+        /**
+         * Akhn ai constructor method er besh kicu gunobali ace. Shegula holo:
+         * Jokoni ai Person class tar object create kora hobe tokonie ai constructor method automatically execute hoye jabe.
+         * ai method ta onno jekhono function/method er moto parameter nei and ogula niye kaj korte pare.
+         * constructor method kintu onno method gular moto kono kicu return korte pare na. and atai er limitation.
+         * And class er moddhe constructor method toiri korte caile atar nam sobsomoy constructor rakthe hobe.
+         */
+        // now let's see ata tikmoto kaj korce kina:
+        console.log("I am a Constructor method.")
+    }
+}
+
+let personObj =new Person();
+
