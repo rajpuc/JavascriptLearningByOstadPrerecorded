4ae422c8e99437abb4f32624598603e918cf68c1
rajesh <ajoypal.raj@gmail.com>
2024-12-15 22:26:39 +0600

    c63: how to work with constructor parameter

---
+++ 63/118.js
@@ -0,0 +1,13 @@
+/**
+ * !Constructor parameter niye kivabe kaj korte hoi.
+ * er jonno nicher example gula dekho:
+ */
+
+class Person{
+    constructor(num1,num2){
+        let sum= num1+num2;
+        console.log(sum);
+    }
+}
+
+let personObj = new Person(10,20);
