fcee7a6c1b0201a8490de386b3d4136981df3b9f
rajesh <ajoypal.raj@gmail.com>
2024-12-20 14:39:17 +0600

    c73: Abstraction

---
+++ 73/138.js
@@ -0,0 +1,22 @@
+/**
+ * !Abstraction
+ * 1. abstract er bangla meaning hoce বিমূর্ত | means jar kono murthi thake na. Shohoj kothai jar kono ostittho thake na, instance thake na ba jar kono object thake na shei bishotakei amra boli abstraction.
+ * 2. abstraction er bishoita ashe inheritance er moddhe teke.nicher example ta keyal koro:
+ */
+
+class Father{
+
+}
+class Son extends Father{
+
+}
+
+
+//3. agher notes inheritance a amra ai example onk use korechi.
+
+let obj1 = new Father();
+let obj2 = new Son();
+//4. amra inheritance toiri korechi bothe, kintu amra chaile babar o obj toiri korte pari and celer to parie.
+//5. Kintu onno programming language like jave/c++,shekhane abstraction namer alada akta bishoi royece and abstract keyword name a alada akta keyword e royece j keyword ta bebohar korle shudu matro child class eri object toiri kora jai kintu parent class er obj toiri kora jai na.
+//6. kintu js a abstract keyword ta nei.
+//7. tai bola jai abstraction er process ta js a nei. 
