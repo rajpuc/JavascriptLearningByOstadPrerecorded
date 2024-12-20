b381b983fd8fa17c1f0714aa0daf31aaf633b54d
rajesh <ajoypal.raj@gmail.com>
2024-12-20 14:18:41 +0600

    72: polymorphism

---
+++ 72/136.js
@@ -0,0 +1,7 @@
+/**
+ * !OOP - Polymorphism
+ * 1. Already amra polymorphism dekhe feleci.
+ * 2. Polymorphism er bangla hocce bohurupota. Akn coding er moddhe jokon apni bohurupota sristy korcen sheta hote pare: inheritance er overriding er maddhome,hote pare overloading er maddhome tokon shei bishoi gulakei amra bolci polymorphism. 
+ * 3. thats why oop er polymorphism er j bishoi guli sheguli amra cover kore felechi.
+ */
+
