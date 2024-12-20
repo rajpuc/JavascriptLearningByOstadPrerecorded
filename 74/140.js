584d33450934686e4eeb398281c8b98e997fd9dd
rajesh <ajoypal.raj@gmail.com>
2024-12-20 18:25:01 +0600

    c75:OOP - Encapsulation

---
+++ 74/140.js
@@ -0,0 +1,14 @@
+/**
+ *!OOP - Encapsulation
+ *1. apni jodi akta oshuder capsuler kotha chinta koren tahole shekhane oshuder joto ingridients royece joto chamical mechanism royece sob gula k sundor kore bind kore akta capsule er moddhe rekhe dewa hoi.Programming er moddheo ai bind kore rakhar process ta ace.
+ *2.Programminge er moddhe amra group of data and group of action niye kaj kori. Apni jokon eki group er data and action bind kore ek jaigai rekhe den shei bishoitakei Encapsulation bola hoi.
+ *3.Encapsulation er besh kichu sujog shubidha royece:
+ *3.1 Hides complexity.
+ *3.2 Data Protection.
+ *3.3 Control Access.
+ *3.4 Enhances Maintainability.
+ *4. Js er vitore amra 3 vabe encaptulation k acieve korte pari:
+ *4.1 Encapsulation using closures.
+ *4.2 Encapsulation using constructor functions.
+ *4.3 Encapsulation using ES6 classes.
+ */
