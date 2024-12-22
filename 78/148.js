25d8a467ba08fb4549ead7bfb2decd6b29417763
rajesh <ajoypal.raj@gmail.com>
2024-12-22 11:58:51 +0600

    c78: Debugging

---
+++ 78/148.js
@@ -0,0 +1,9 @@
+/**
+ * ! Debugging Module 11 video no: 23
+ * Aikhan teke ja shikeci :
+ * 1. Debugging using Console.log. [video time-> 6: 40]
+ * 2. Debugging using Browser Debug Tool.[video time-> 8: 30] important
+ * 3. Debugging using Browser Debugger keyword.[video time-> 23: 40] [kewyord ti hocce-> debugger]
+ * 4. Explore Browser Network Tab.[video time-> 35: 50]
+ * 5. Debugging on Node JS Runtime.[video time-> 42: 10] important
+ */
